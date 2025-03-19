5c79cd8302b839a0dedfff98f661a0e6d210b2c2
Peter Wang <ipeter.wang@outlook.com>
2025-03-19 21:13:39 +0800

    change: reset blog

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Peter Wang';
+    : 'Jay Doe';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : '寻找真我';
+    : 'Next.js Blog Theme';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
